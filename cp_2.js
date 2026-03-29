ae56241e26c4a42dfe5b243966b0a6c7172b0788
Rrelle <rrelleferguson@usf.edu>
2026-03-29 19:45:00 -0400

    created the function in java to display more options

--- cp_2.js
+++ cp_2.js
@@ -22,3 +22,20 @@ async function fetchProductsAsync() {
     } catch(error) {
       handleError(error);
     }           
+function displayProducts(products) {
+    const container = document.getElementById("product-container");
+    products.slice(0, 5).forEach((product) => {
+        const card = document.createElement("div");
+        card.classList.add("product-card");
+
+        const name =product.fields.name;
+        const price = product.fields.price;
+        const image = product.fields.image[0].url;
+        card.innerHTML = `
+        h3>${name}</h3>
+        img src="${image}" alt="${name}">
+        p>Price: $${price}</p>
+        `;
+        container.appendChild(card);
+    });
+}
